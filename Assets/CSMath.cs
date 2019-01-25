52a01c784299b4a56bd61320146a30ed659dfcb9
shshwdr <cs.vivian04@gmail.com>
2019-01-25 02:05:00 -0800

    check if two lines will intersect

--- Assets/CSMath.cs
+++ Assets/CSMath.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class CSMath : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static bool IsLineToLineIntersect(Vector3 line1Start,Vector3 line1End, Vector3 line2Start,Vector3 line2End,out Vector3 intersection)
     {
-        
-    }
+        Vector3 line1Dir = line1End - line1Start;
+        Vector3 line2Dir = line2End - line2Start;
+        Vector3 line3 = line1Start - line2Start;//any two position on 1 and 2 would work
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Vector3 linePerpen12 = Vector3.Cross(line1Dir, line2Dir);
+        //if l1 and l2 parallel, area should be 0
+        bool isParallel = Mathf.Approximately(linePerpen12.sqrMagnitude, 0);
+        if (isParallel)
+        {
+            if (line1Start == line2Start)
+            {
+                Debug.Log("two lines are overlap");
+
+                intersection = Vector3.zero;
+                return false;
+            }
+            Debug.Log("two lines parallel to each other");
+            intersection = Vector3.zero;
+            return false;
+        }
+        //find the line4 perpendicular to l1 and l2
+        //if l4 also perpendicular to l3, then l1 and l2 on the same plane
+        float line3DotLinePerpen12 = Vector3.Dot(line3, linePerpen12);
+        bool isCorplaner = Mathf.Approximately(line3DotLinePerpen12,0);
+        if (!isCorplaner)
+        {
+            Debug.Log("two lines are not on the same plane");
+            intersection = Vector3.zero;
+            return false;
+        }
+        Debug.Log("two lines intersect");
+        intersection = Vector3.zero;
+        return true;
     }
 }
--- Assets/LineIntersection/LineIntersection.cs
+++ Assets/LineIntersection/LineIntersection.cs
@@ -5,62 +5,24 @@ using UnityEngine;
 public class LineIntersection : MonoBehaviour
 {
 
-    public Transform playerTrans;
-    public Transform planeTrans;
-    public LineRenderer lineRenderer;
+    public LineRenderer line1;
+    public LineRenderer line2;
+    public GameObject intersectionObject;
     //player want to detect if an enemy is in front of it
     void Update()
     {
-        //ref http://www.scratchapixel.com/lessons/3d-basic-rendering/minimal-ray-tracer-rendering-simple-shapes/ray-plane-and-ray-disk-intersection
-
-        //how to define a plane:
-        //p,p0 are two points on plane, n is the normal of plane
-        //(p-p0)*n = 0 (* is dot)
-
-        //how to define a ray:
-        //l0 is the origin, p is the point intersect with plane, l is the direction of ray, t is the length of ray
-        //l0 + t*l = p
-
-        //(l0+t*l-p0)*n = 0
-        //t = (p0*n-l0*n)/(l*n)
-
-        Vector3 p0 = planeTrans.position;//any point on plane would work
-        Vector3 n = -planeTrans.up;//assume player is above plane
-
-        Vector3 l0 = playerTrans.position;
-        Vector3 l = playerTrans.forward;
-
-        
-
-
-        //take care the case when player face direction perpendicular to n, therefore no intersection
-        if(Mathf.Approximately(Vector3.Dot(l, n), 0))
+        Vector3 intersection;
+        if(line1.positionCount==2 && line2.positionCount == 2)
         {
-            Debug.Log("no intersection");
-        }
-        else
-        {
-            float t = (Vector3.Dot(p0, n) - Vector3.Dot(l0, n)) / Vector3.Dot(l, n);
-            Vector3 p = l0 + t * l;
-            lineRenderer.SetPosition(0, l0);
-            lineRenderer.SetPosition(1, p);
 
-            //unity version verify
-            Ray ray = new Ray(l0, l);
-            Plane plane = new Plane(n, p0);
-            float distance;
-            if (plane.Raycast(ray, out distance))
+            if(CSMath.IsLineToLineIntersect(line1.GetPosition(0), line1.GetPosition(1), line2.GetPosition(0), line2.GetPosition(1), out intersection))
             {
-                Debug.Assert(Mathf.Approximately(distance, t),"distance calculated from Unity "+distance +"does not equal to the one calculated from my math");
-                //Debug.Log(distance+" "+t);
+                intersectionObject.SetActive(true);
+            }
+            else
+            {
+                intersectionObject.SetActive(false);
             }
         }
-
-
-    }
-
-    float Dot(Vector3 v1,Vector3 v2)
-    {
-        return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
     }
 }

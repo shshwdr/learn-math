1fd470285b732e1ef1c7721db1e1239b35716e34
shshwdr <cs.vivian04@gmail.com>
2019-03-02 23:52:13 -0800

    add look at

---
+++ Assets/LookAt/LookAt.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAt : MonoBehaviour
+{
+    public GameObject target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 targetP= target.transform.position;
+        Vector3 position = transform.position;
+        Vector3 forward = targetP - position;
+        Vector3 up = Vector3.up;
+        Vector3 right = Vector3.Cross(forward, up);
+        up = Vector3.Cross(right,forward);
+        Matrix4x4 t = new Matrix4x4();
+        for (int i = 0; i < 3; i++)
+        {
+            t[i, 0]  = right[i];
+            t[i, 1] = up[i];
+            t[i, 2] = forward[i];
+        }
+        transform.rotation = t.rotation;
+    }
+}

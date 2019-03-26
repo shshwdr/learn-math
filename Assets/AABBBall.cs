fd0576168294f15b72316e2ddb509ca662a5600d
shshwdr <cs.vivian04@gmail.com>
2019-03-26 01:37:12 -0700

    start working on aabb

---
+++ Assets/AABBBall.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AABBBall : MonoBehaviour
+{
+    public float boardHeight = 20;
+    public float boardWidth = 20;
+    public float height = 1;
+    public float width = 1;
+    float[] vertices = { };
+    public Vector3 moveDir = new Vector3(0.1f,0.2f,0);
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    bool CheckBounds()
+    {
+        return false;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (!CheckBounds())
+        {
+            transform.position += moveDir;
+        }
+    }
+}
